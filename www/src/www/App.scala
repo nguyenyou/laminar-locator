c9d8141c4241ed4a385c283d944e7436a408d820
nguyenyou <tunt081295@gmail.com>
2025-07-11 23:53:43 +0700

    .

--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -2,10 +2,30 @@ package www
 
 import www.components.{Button, Card}
 import www.locator.UIComponent
+import www.locator.component
+import www.locator.UIComponentSource.withSourcePath
 
 import com.raquo.laminar.api.L.*
 
 case class App() extends UIComponent {
+  @component
+  def renderLabel(): HtmlElement = {
+    div(
+      "hello"
+    ).withSourcePath
+  }
+
+  @component
+  def renderStyledButton(): HtmlElement = {
+    button(
+      "Styled Button",
+      backgroundColor("lightblue"),
+      padding.px(10),
+      borderRadius.px(5),
+      border("none"),
+      cursor.pointer
+    ).withSourcePath
+  }
   def render() = {
     div(
       width.px(600),
@@ -18,7 +38,14 @@ case class App() extends UIComponent {
       Card("Example Card")(
         p("This is some content inside the card."),
         Button()(),
-        p("More content here.")
+        p("More content here."),
+        // Demonstrate the @uicomponent annotation usage
+        renderLabel().amend(
+          color("blue"),
+          fontSize.px(16),
+          fontWeight.bold
+        ),
+        renderStyledButton()
       )
     )
   }
